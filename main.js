fb507753e2b726ae979111ae33740b7e8f8cc1d4
NoshyAymanElozere <114044471+NoshyAymanElozere@users.noreply.github.com>
2022-10-04 00:28:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+$('.clc1').click(function(){
+    $('.image').html('<img src="imeges/0.png" >')
+ $('.container').attr('style','background-color:#000')
+})
+//////////secnd/////////////
+$('.clc2').click(function(){
+    $('.image').html('<img src="imeges/1.png" >')
+ $('.container').attr('style','background-color:#247ec8')
+})
+//////////////third///////////
+$('.clc3').click(function(){
+    $('.image').html('<img src="imeges/2.png" >')
+ $('.container').attr('style','background-color:#1e1e1e')
+})
+////////////////////fours/////////
+$('.clc4').click(function(){
+    $('.image').html('<img src="imeges/3.png" >')
+ $('.container').attr('style','background-color:#c79b53')
+})
+$('.clc5').click(function(){
+    $('.image').html('<img src="imeges/4.png" >')
+ $('.container').attr('style','background-color:#c82525')
+})
+
